c488995c095bf640beee1886d54423f02684000e
angs0003 <angs0003@flinders.edu.au>
2024-06-13 21:08:57 +0930

    first react App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useEffect, useState } from 'react';
+import './App.css'
+import MovieCard from './MovieCard.jsx'
+import SearchIcon from './search.svg'
+//830ba576
+
+const API_URL = 'http://www.omdbapi.com?apikey=830ba576'
+
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        searchMovies('Shrek');
+    }, [])
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+
+    }
+    
+    return (
+        <div className="app">
+            <h1>Movieland</h1>
+
+            <div className="search">
+                <input placeholder="Search for movies" 
+                value={searchTerm}
+                onChange={(e) => { setSearchTerm(e.target.value)}}>
+
+                </input>
+                <img src={SearchIcon} 
+                alt="search icon" 
+                onClick={() => { searchMovies(searchTerm) }}>
+                </img>
+            </div>
+
+            {
+                movies?.length > 0 ?
+                    (
+                        <div className="container">
+                            {movies.map((movie) => (
+                                <MovieCard movie={movie} />
+                            ))}
+
+                        </div>
+                    ) : (
+                        <div className="empty">
+                            <h2>No movies found</h2>
+                        </div>
+                    )
+            }
+
+        </div>
+
+    )
 }
 
 export default App;
---
+++ src/test/test.js
@@ -0,0 +1,23 @@
+const pics = {
+    panda: "http://bit.ly/1Tqltv5",
+    owl: "http://bit.ly/1XGtkM3",
+    owlCat: "http://bit.ly/1Upbczi"  
+};
+
+const panda = (
+    <img 
+    src={pics.panda}
+    alt="Lazy Panda" />
+);
+
+const owl = (
+    <img 
+    src = {pics.owl}
+    alt = "Wise owl"/>
+);
+
+const owlCat = (
+    <img 
+    src = {pics.owl}
+    alt = "An Abomination"/>
+);
